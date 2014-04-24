5122ab92195ff475f4261f0c26948be6b6d9cafc
Collin J. Sutton <collin.sutton@gmail.com>
2014-04-23 17:20:55 -0700

    scripts/hydra_drive_logic.py: correct issue with Python references

--- hydra_drive/scripts/hydra_drive_logic.py
+++ hydra_drive/scripts/hydra_drive_logic.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import sys
 import rospy
@@ -6,29 +8,37 @@ from hydra_drive.msg import PowerLevels
 
 TIMEOUT = 1.0                   # one second
 
-last_receive_time = None
-message = None
 
-def handle_power_received(msg):
-    last_receive_time = rospy.get_time()
-    message = msg
+class Data(object):
+    def __init__(self):
+        self.message = PowerLevels()
+        self.timeout = None
+
+rospy.init_node('hydra_drive_logic')
+
+data = Data()
+pub = rospy.Publisher('power', PowerLevels)
+
+def halt(event):
+    data.message.left = 0.0
+    data.message.right = 0.0
+    pub.publish(data.message)
+
+def handle_joystick_received(msg):
+    if data.timeout:
+        data.timeout.shutdown()
+    data.timeout = rospy.Timer(rospy.Duration(TIMEOUT), halt, True)
+    data.message = msg
+
+sub = rospy.Subscriber('joystick', PowerLevels, handle_joystick_received)
+
 
 def run():
-    rospy.init_node('hydra_drive_logic')
-
-    pub = rospy.Publisher('power', PowerLevels)
-    sub = rospy.Subscriber('joystick', PowerLevels, handle_power_received)
-    message = PowerLevels()
-
     rospy.loginfo('hydra_drive_logic initialized.')
 
     r = rospy.Rate(50)
     while not rospy.is_shutdown():
-        if last_receive_time is None or rospy.get_time() - last_receive_time >= TIMEOUT:
-            message.left = 0
-            message.right = 0
-        pub.publish(message)
-            
+        pub.publish(data.message)
         r.sleep()
 
 if __name__ == '__main__':
