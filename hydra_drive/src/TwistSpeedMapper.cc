b1db4d865a33b7ec8603ba85d713f3983310fcc7
Collin J. Sutton <collin.sutton@gmail.com>
2014-05-02 15:49:05 -0700

    fixes to hydra_drive files RE: TwistSpeedMapper nodelet-plugin

--- hydra_drive/src/TwistSpeedMapper.cc
+++ hydra_drive/src/TwistSpeedMapper.cc
@@ -24,6 +24,7 @@
 #define PUBLISHER_QUEUE_SIZE 128
 
 PLUGINLIB_DECLARE_CLASS(hydra_drive, TwistSpeedMapper, hydra_drive::TwistSpeedMapper, nodelet::Nodelet)
+PLUGINLIB_EXPORT_CLASS(hydra_drive::TwistSpeedMapper, nodelet::Nodelet)
 
 namespace hydra_drive
 {
