52278b88a6d7e6d44934dc1d652affba1769c83e
Collin J. Sutton <collin.sutton@gmail.com>
2014-05-02 16:07:58 -0700

    hydra_drive: Arduino code update
      * added `float` version of `map` so things actually work
      * added publisher for mapped power levels for debugging

--- hydra_drive/arduino/hydra_drive/hydra_drive.ino
+++ hydra_drive/arduino/hydra_drive/hydra_drive.ino
@@ -22,25 +22,38 @@
 #define PIN_LEFT 9
 #define PIN_RIGHT 10
 
-#define MIN_PULSE_WIDTH 1001
-#define NEUTRAL_PULSE_WIDTH 1500
-#define MAX_PULSE_WIDTH 2041
+#define MIN_PULSE_WIDTH 1001.0
+#define NEUTRAL_PULSE_WIDTH 1500.0
+#define MAX_PULSE_WIDTH 2041.0
 
 static void power_cb(const hydra_drive::PowerLevels& msg);
 
+static hydra_drive::PowerLevels mapped;
 static ros::NodeHandle  nh;
 static ros::Subscriber<hydra_drive::PowerLevels> subscription ( "power", power_cb );
+static ros::Publisher publisher ( "mapped", &mapped );
 static Servo left, right;
 
+static float
+map(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
 static void power_cb(const hydra_drive::PowerLevels& msg) {
-  left.writeMicroseconds(map(msg.left, -1.0, 1.0, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH));
-  right.writeMicroseconds(map(msg.right, -1.0, 1.0, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH));
+  mapped.left = map(msg.left, -1.0, 1.0, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  mapped.right = map(msg.right, -1.0, 1.0, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  left.writeMicroseconds((int) mapped.left);
+  right.writeMicroseconds((int) mapped.right);
+  publisher.publish(&mapped);
 }
 
 void setup(){
   nh.initNode();
   
   nh.subscribe(subscription);
+  nh.advertise(publisher);
 
   pinMode(PIN_LEFT, OUTPUT);
   pinMode(PIN_RIGHT, OUTPUT);
